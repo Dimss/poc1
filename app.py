8e527541f433c257faafa7ac5cf1ee8dd3fdd5f9
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-09 15:28:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body="www.gov.il,10")
-channel.basic_publish(exchange='', routing_key='hello', body="www.pmo.gov.il,10]")
-channel.basic_publish(exchange='', routing_key='hello', body="www.police.gov.il,10]")
-channel.basic_publish(exchange='', routing_key='hello', body="www.president.gov.il,10]")
-channel.basic_publish(exchange='', routing_key='hello', body="www.shabak.gov.il,10]")
-channel.basic_publish(exchange='', routing_key='hello', body="www.mossad.gov.il,10]")
+channel.basic_publish(exchange='', routing_key='hello', body="https://www.gov.il,10")
+channel.basic_publish(exchange='', routing_key='hello', body="https://www.pmo.gov.il,10]")
+channel.basic_publish(exchange='', routing_key='hello', body="https://www.police.gov.il,10]")
+channel.basic_publish(exchange='', routing_key='hello', body="https://www.president.gov.il,10]")
+channel.basic_publish(exchange='', routing_key='hello', body="https://www.shabak.gov.il,10]")
+channel.basic_publish(exchange='', routing_key='hello', body="https://www.mossad.gov.il,10]")
 
 print(" [x] Queue initialized")
 connection.close()
