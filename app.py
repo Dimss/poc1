54adeaa57462beaf41b2e882f29eb7725020a26f
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-10 13:06:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import pika
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.check-sites.svc.cluster.local'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
+
+with open('SiteList.json') as f:
+    data = json.load(f)
+
+for i in range(10):
+    channel.basic_publish(exchange='', routing_key='hello', body=data[i])
+    print("published data[{0}] = {1}".format(i, data[i]))
 
 channel.basic_publish(exchange='', routing_key='hello', body="https://www.google.com")
 channel.basic_publish(exchange='', routing_key='hello', body="https://www.cnn.com")
