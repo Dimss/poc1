2fadc085c945a93ea22013b6729202330030304b
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-13 17:00:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.check-sites.svc.cluster.local'))
 channel = connection.channel()
 
-print("NEW VERSION!!!")
+print("NEW VERSION!!! serialized the json payload")
 
 channel.queue_declare(queue='hello')
 
@@ -14,8 +14,9 @@ data = json.loads(requests.get('https://raw.githubusercontent.com/abarlev/poc1/m
 
 for i in range(100):
     print("published data[{0}] = {1}".format(i, data[i]))
-    channel.basic_publish(exchange='', routing_key='hello', body=data[i])
-    print("published data[{0}] = {1}".format(i, data[i]))
+    body=json.dumps(data[i])
+    channel.basic_publish(exchange='', routing_key='hello', body=body))
+    print("published data[{0}] = {1}".format(i, body))
 
 #channel.basic_publish(exchange='', routing_key='hello', body="https://www.google.com")
 #channel.basic_publish(exchange='', routing_key='hello', body="https://www.cnn.com")
