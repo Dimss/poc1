28b8c8ce14aaeaca48375939a34eab3c692f5b4b
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-13 15:00:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
 import pika
 import json
+import requests
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.check-sites.svc.cluster.local'))
 channel = connection.channel()
 
-print("NEW VERSION!!!")
+#print("NEW VERSION!!!")
 
 channel.queue_declare(queue='hello')
 
-with open('SiteList.json') as f:
-    data = json.load(f)
+data = json.loads(requests.get('https://github.com/abarlev/poc1/edit/master/SiteList.json').text)
 
-for i in range(10):
+for i in range(100):
     channel.basic_publish(exchange='', routing_key='hello', body=data[i])
     print("published data[{0}] = {1}".format(i, data[i]))
 
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.google.com")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.cnn.com")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.ynet.co.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.google.com")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.cnn.com")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.ynet.co.il")
 
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.gov.il")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.pmo.gov.il")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.police.gov.il")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.president.gov.il")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.shabak.gov.il")
-channel.basic_publish(exchange='', routing_key='hello', body="https://www.mossad.gov.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.gov.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.pmo.gov.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.police.gov.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.president.gov.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.shabak.gov.il")
+#channel.basic_publish(exchange='', routing_key='hello', body="https://www.mossad.gov.il")
 
 print(" [x] Queue initialized")
 connection.close()
