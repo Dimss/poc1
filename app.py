227c2d83201e8f61a640332c671e06a21a71c9bf
Dmitry Kartsev <dkartsev@redhat.com>
2019-02-14 11:59:59 +0200

    add html test runner

--- app.py
+++ app.py
@@ -2,18 +2,34 @@
 import pika
 import json
 import requests
+import logging
+import sys
+import conf
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.check-sites.svc.cluster.local'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=conf.RABBITMQ_IP))
 channel = connection.channel()
+channel.queue_declare(queue=conf.RABBITMQ_QUEUE)
 
-channel.queue_declare(queue='sites')
 
-data = json.loads(requests.get('https://raw.githubusercontent.com/abarlev/poc1/master/SiteList.json').text)
+def publish_message(message):
+    channel.basic_publish(exchange='', routing_key=conf.RABBITMQ_QUEUE, body=message)
+    logging.info("published row = {0}".format(message))
 
-for row in data:
-    serialized = json.dumps(row)
-    channel.basic_publish(exchange='', routing_key='sites', body=serialized)
-    print("published row = {0}".format(serialized))
 
-print(" [x] Queue initialized")
-connection.close()
+def main():
+    data = requests.get(conf.SITES_LIST).json()
+    for row in data:
+        serialized = json.dumps(row)
+        publish_message(serialized)
+
+    logging.info(" [x] Queue initialized")
+    connection.close()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(asctime)s %(levelname)s] %(message)s',
+        stream=sys.stdout
+    )
+    main()
